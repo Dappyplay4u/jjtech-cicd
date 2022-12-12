fdf8148a3fc1be158f25827b5579dd67ab07542b
Dappyplay4u <jegede.oladapo@ymail.com>
2022-12-11 21:07:28 -0500

    techmax updat

--- ec3/output.tf
+++ ec3/output.tf
@@ -1,7 +1,7 @@
-output "webinstance_id" {
+output "web_instance_id" {
     value = aws_instance.web_instance.id
 }
 
-output "webinstance_public_ip" {
+output "web_instance_public_ip" {
     value = aws_instance.web_instance.public_ip
 }
