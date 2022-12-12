b287daa5b11cb585888061c948a28f6d23cb140a
Dappyplay4u <jegede.oladapo@ymail.com>
2022-12-11 21:02:26 -0500

    techmax updat

--- main.tf
+++ main.tf
@@ -39,6 +39,6 @@ module "ec3_web" {
   source = "./ec3"
   name   = "web"
   tags          = local.common_tags
-  iam_role_name = module.iam.ec3_iam_role_name
+  iam_role_name = module.iam.ec2_iam_role_name
   key_pair_name = var.key_pair_name
 }
