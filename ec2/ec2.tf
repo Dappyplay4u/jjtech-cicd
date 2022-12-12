bf201c6783042d1727410771eda200ab214ca7be
Dappyplay4u <jegede.oladapo@ymail.com>
2022-12-11 19:34:27 -0500

    techmax updat

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,8 +22,7 @@ variable "key_pair_name" {
 resource "aws_security_group" "ec2_sg" {
   name        = join("", [var.name, "-", "ec2-sg"])
   description = "Allow  traffic for http and ssh"
-
-
+  
   ingress {
     from_port   = 22
     to_port     = 22
