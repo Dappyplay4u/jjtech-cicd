69fd979a3c2a892a43461aee840f15e2062bf6b4
Dappyplay4u <jegede.oladapo@ymail.com>
2022-12-11 20:58:20 -0500

    techmax updat

--- ec2/ec3.tf
+++ ec2/ec3.tf
@@ -1,24 +1,3 @@
-variable "name" {
-  type        = string
-  description = "name tag value"
-}
-
-variable "tags" {
-  type        = map(any)
-  description = "tags for the vpc module"
-}
-
-variable "iam_role_name" {
-  type        = string
-  description = "iam role name to attach to the instance profile"
-}
-
-variable "key_pair_name" {
-  type        = string
-  description = "keypair to utilize"
-
-}
-
 resource "aws_security_group" "ec3_sg" {
   name        = join("", [var.name, "-", "ec3-sg"])
   description = "Allow  traffic for http and ssh"
